358f640a0c05a7e7abc80694e7efe06d7583c7b8
palina bushmovich <bushmovichp@gmail.com>
2018-08-20 23:21:02 +0300

    added decorator pattern

---
+++ TestWebProject/PageObject/Decorator/HomePageDecorator.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using TestWebProject.Driver;
+
+namespace TestWebProject.PageObject
+{
+    public class HomePageDecorator : IHomePage
+    {
+        IHomePage component;
+
+        private static readonly By _loginInputBy = By.CssSelector("input[type ='email']");
+
+        public HomePageDecorator(IHomePage component) { this.component = component; }
+
+        public LogInForm OpenLoginForm()
+        {
+           LogInForm logInform = component.OpenLoginForm();
+
+            logInform.LogInInput.HighlightElement(_loginInputBy);
+
+            return logInform;
+        }
+    }
+}
---
+++ TestWebProject/Tests/MultipleUsersLogIn.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestWebProject.Driver;
+using TestWebProject.PageObject;
+
+namespace TestWebProject.Tests
+{
+    [TestClass]
+    public class MultipleUsersLogIn : BaseTest
+    {
+        private HomePage _homePage;
+        private MainNavigationPanel _navigationPanel;
+        private MainEmailBoxPage _mainEmailBoxPage;
+        private SentMailPage _sentMailPage;
+        private LogInForm _logInform;
+
+        private static TestContext _testContextInstance;
+
+        [ClassInitialize()]
+        public static void SendEmailTestsInitialize(TestContext context)
+        {
+            Browser = Browser.Instance;
+            Browser.WindowMaximize();
+            Browser.NavigateTo(Configurations.StartUrl);
+            _testContextInstance = context;
+        }
+       
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "LogInData.xml", "Credentials", DataAccessMethod.Sequential)]
+        [DeploymentItem("Data")]
+        public void LogIn_VerifySuccessfulLogIn()
+        {
+            #region set variables
+
+            string input = _testContextInstance.DataRow["InputParameter"].ToString();
+            string expectedResult = _testContextInstance.DataRow["Result"].ToString();
+
+            #endregion
+
+            _homePage = new HomePage();
+
+            _logInform = _homePage.OpenLoginForm();
+
+            //Log in as first user
+            _mainEmailBoxPage = _logInform.LogInToEmailBox(Constants.Sender, Constants.Password);
+
+            _navigationPanel = new MainNavigationPanel();
+
+            //Verify that login is successful
+            bool isFirstLoginSuccessfull = _navigationPanel.InboxLink.Displayed;
+            Assert.IsTrue(isFirstLoginSuccessfull, $"Login of first user '{Constants.Sender}' was not successful");
+
+            _logInform = _mainEmailBoxPage.SignOut();
+
+            _logInform.LogInToEmailBox(Constants.Recipient, Constants.Password);
+
+        }
+
+    }
+}
