b5f6d82ba9930da87e01afb7d5988e89bc35785a
Palina Bushmovich <palina_bushmovich@epam.com>
2018-08-23 20:17:18 +0300

    added method for read json file

---
+++ TestWebProject/BDD/JsonItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebProject.BDD
+{
+    public class JsonItems
+    {
+        public string Username;
+        public string Password;
+        public string Text;
+    }
+}
---
+++ TestWebProject/BDD/LoadJson.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebProject.BDD
+{
+    public class LoadJson
+    {
+  
+        public void ReadJson()
+        {
+            using (StreamReader r = new StreamReader("Data.json"))
+            {
+                string json = r.ReadToEnd();
+                List<JsonItems> items = JsonConvert.DeserializeObject<List<JsonItems>>(json);
+            }
+        }
+
+    }
+}
