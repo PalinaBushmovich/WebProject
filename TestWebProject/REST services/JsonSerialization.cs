0e5cd7a7ad8bd9f8eb379a306f807a740db3c90b
palina bushmovich <bushmovichp@gmail.com>
2018-08-30 00:58:16 +0300

    added infrastructure for testing web-services

---
+++ TestWebProject/REST services/JsonSerialization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace TestWebProject.REST
+{
+    public static class JsonSerialization
+    {
+        public static string SerializeToJson<T>(T obj)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string json = js.Serialize(obj);
+
+            return json;
+        }
+
+        public static T DeserializeFromJson<T>(string json)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            T caseGetResponse = caseGetResponse = serializer.Deserialize<T>(json);
+            return caseGetResponse;
+        }
+    }
+}
---
+++ TestWebProject/REST services/TestsForWebServices.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using TestWebProject.REST.REST_dto;
+
+namespace TestWebProject.REST
+{
+    [TestClass]
+    public class TestsForWebServices
+    {
+        private string Url = "https://jsonplaceholder.typicode.com/users";
+
+        [TestMethod]
+        public void VerifyResponceStatusCode()
+        {
+            HttpStatusCode responceCode = HTTPrequests.GetResponseStatusCode(HTTPrequests.ExecuteGetRequest(Url));
+
+        }
+        [TestMethod]
+        public void SendPostUserRequest()
+        {
+            string userRequest = JsonSerialization.SerializeToJson(new UserGETdto());
+            HttpStatusCode statusCode = HTTPrequests.GetResponseStatusCode(HTTPrequests.ExecutePostRequest(Url, userRequest));
+            
+        }
+    }
+}
